ea004eba8eaf5eb83bdef0a9b1c00880ad49ac71
Oliver Leis <oliver.leis@gmail.com>
2023-07-04 17:33:35 +0200

    minor

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 ~/.fehbg &
+/usr/libexec/xfce4/notifyd/xfce4-notifyd &
 picom -b &
 ~/Src/dwm/scripts/sched &
 ~/Dev/bash/weather &
 ~/Dev/bash/radio &
 nm-applet &
 udiskie -t &
+xfce4-power-manager &
+blueman-applet
 xrandr --auto --output DP-2 --mode 1920x1080 --right-of DP-0
-/usr/libexec/xfce4/notifyd/xfce4-notifyd &
 setxkbmap -layout de,gr -option caps:escape
