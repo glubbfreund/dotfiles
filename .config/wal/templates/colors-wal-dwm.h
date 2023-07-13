f2a468158004d54a7f2a504a08a322f78baeabe4
Oliver Leis <oliver.leis@gmail.com>
2023-07-13 11:42:46 +0200

    theme change

--- .config/wal/templates/colors-wal-dwm.h
+++ .config/wal/templates/colors-wal-dwm.h
@@ -12,7 +12,7 @@ static const char special2[] = "{color3}";
 static const char *colors[][3]      = {{
     /*               fg           bg         border                         */
     [SchemeNorm] = {{ norm_fg,     norm_bg,   norm_border }}, // unfocused wins
-    [SchemeSel]  = {{ norm_bg,      special2,    sel_bg }},  // the focused win
+    [SchemeSel]  = {{ norm_bg,      sel_bg,    sel_bg }},  // the focused win
     [SchemeMark]  = {{ special,      norm_bg,    special }},  // the focused win
     [SchemeTitle] =  {{ sel_bg,      norm_bg,    norm_border }},
 }};
