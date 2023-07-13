995c0ee1439b11acb8095f8fdee8ab96ea328d7e
Oliver Leis <oliver.leis@gmail.com>
2023-07-13 12:49:28 +0200

    minor change

--- .config/wal/templates/colors-wal-dwm.h
+++ .config/wal/templates/colors-wal-dwm.h
@@ -7,13 +7,13 @@ static const char sel_bg[] = "{color2}";
 static const char sel_border[] = "{color15}";
 
 static const char special[] = "{color9}";
-static const char special2[] = "{color3}";
+static const char special2[] = "#D4AF37";
 static const char def_bg[] = "#111111";
 
 static const char *colors[][3]      = {{
     /*               fg           bg         border                         */
     [SchemeNorm] = {{ norm_fg,     norm_bg,   norm_border }}, // unfocused wins
     [SchemeSel]  = {{ norm_bg,      sel_bg,    sel_bg }},  // the focused win
-    [SchemeMark]  = {{ special,      norm_bg,    special2 }},  // the focused win
+    [SchemeMark]  = {{ special2,      norm_bg,    special2 }},  // the focused win
     [SchemeTitle] =  {{ sel_bg,      norm_bg,    norm_border }},
 }};
