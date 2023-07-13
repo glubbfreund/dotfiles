ecd2958e468131d96f1f47b5213f55a2b18270a2
Oliver Leis <oliver.leis@gmail.com>
2023-07-13 09:04:53 +0200

    changed theme

--- .config/wal/templates/colors-wal-dmenu.h
+++ .config/wal/templates/colors-wal-dmenu.h
@@ -1,8 +1,8 @@
 static const char *colors[SchemeLast][2] = {{
 	/*     fg         bg       */
 	[SchemeNorm] = {{ "{color15}", "{color0}" }},
-	[SchemeSel] = {{ "{color15}", "{color2}" }},
-	[SchemeSelHighlight] = {{ "{color9}", "{color2}" }},
+	[SchemeSel] = {{ "{color0}", "{color3}" }},
+	[SchemeSelHighlight] = {{ "{color0}", "{color3}" }},
 	[SchemeNormHighlight] = {{ "{color1}", "{color0}" }},
 	[SchemeOut] = {{ "{color15}", "{color14}" }},
 }};
--- .config/wal/templates/colors-wal-dwm.h
+++ .config/wal/templates/colors-wal-dwm.h
@@ -7,11 +7,12 @@ static const char sel_bg[] = "{color2}";
 static const char sel_border[] = "{color15}";
 
 static const char special[] = "{color9}";
+static const char special2[] = "{color3}";
 
 static const char *colors[][3]      = {{
     /*               fg           bg         border                         */
     [SchemeNorm] = {{ norm_fg,     norm_bg,   norm_border }}, // unfocused wins
-    [SchemeSel]  = {{ sel_fg,      sel_bg,    sel_bg }},  // the focused win
+    [SchemeSel]  = {{ norm_bg,      special2,    sel_bg }},  // the focused win
     [SchemeMark]  = {{ special,      norm_bg,    special }},  // the focused win
     [SchemeTitle] =  {{ sel_bg,      norm_bg,    norm_border }},
 }};
